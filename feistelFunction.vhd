59a41891ea98a9abd078679232de79d078df4dff
Dean Chester <dean@maquib.com>
2014-02-05 20:19:33 +0000

    Fixed all errors in quartus.

--- feistelFunction.vhd
+++ feistelFunction.vhd
@@ -88,21 +88,23 @@ architecture arch of feistelFunction is
 			permOUT : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 		) ;
 	end component;
-	begin
 	
 	signal e, xorOUT : std_LOGIC_VECTOR(47 DOWNTO 0);
 	signal sBoxesOUT : STD_LOGIC_VECTOR(31 DOWNTO 0);
-	eFunc : eFunction port map (fIn, e);
-	xorBIT : XORBIT48 port map (e, K, xorOUT); 
 	
-	s1 : s1_box port map (xorOUT(47 DOWNTO 42), sBoxesOUT(31 DOWNTO 28));
-	s2 : s2_box port map (xorOUT(41 DOWNTO 36), sBoxesOUT(27 DOWNTO 24));
-	s3 : s3_box port map (xorOUT(35 DOWNTO 30), sBoxesOUT(23 DOWNTO 20));
-	s4 : s4_box port map (xorOUT(29 DOWNTO 24), sBoxesOUT(19 DOWNTO 16));
-	s5 : s5_box port map (xorOUT(23 DOWNTO 18), sBoxesOUT(15 DOWNTO 12));
-	s6 : s6_box port map (xorOUT(17 DOWNTO 12), sBoxesOUT(11 DOWNTO 8));
-	s7 : s7_box port map (xorOUT(11 DOWNTO 6), sBoxesOUT(7 DOWNTO 4));
-	s8 : s8_box port map (xorOUT(5 DOWNTO 0), sBoxesOUT(3 DOWNTO 0));
+	begin
+		
+		eFunc : eFunction port map (fIn, e);
+		xorBIT : XORBIT48 port map (e, K, xorOUT); 
 	
-	pFunc : permutationFunction PORT MAP (sBoxesOUT, fOut);
+		s1 : s1_box port map (xorOUT(47 DOWNTO 42), sBoxesOUT(31 DOWNTO 28));
+		s2 : s2_box port map (xorOUT(41 DOWNTO 36), sBoxesOUT(27 DOWNTO 24));
+		s3 : s3_box port map (xorOUT(35 DOWNTO 30), sBoxesOUT(23 DOWNTO 20));
+		s4 : s4_box port map (xorOUT(29 DOWNTO 24), sBoxesOUT(19 DOWNTO 16));
+		s5 : s5_box port map (xorOUT(23 DOWNTO 18), sBoxesOUT(15 DOWNTO 12));
+		s6 : s6_box port map (xorOUT(17 DOWNTO 12), sBoxesOUT(11 DOWNTO 8));
+		s7 : s7_box port map (xorOUT(11 DOWNTO 6), sBoxesOUT(7 DOWNTO 4));
+		s8 : s8_box port map (xorOUT(5 DOWNTO 0), sBoxesOUT(3 DOWNTO 0));
+		
+		pFunc : permutationFunction PORT MAP (sBoxesOUT, fOut);
 end architecture ; -- arch
