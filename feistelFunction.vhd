18c6a18910f8fd2305fb1db56a6ed1254a89fd16
Dean Chester <dean@maquib.com>
2014-02-05 19:17:49 +0000

    Fixed syntax errors.

--- feistelFunction.vhd
+++ feistelFunction.vhd
@@ -22,56 +22,56 @@ architecture arch of feistelFunction is
 	component s8_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s7_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s6_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s5_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s4_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s3_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
 	component s2_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 
 	component s1_box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
-			sOUT: OUT std_logic_VECTOR(3 downto 0))
+			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
