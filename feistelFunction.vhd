bba878dc109a308999bc4e7febc927c6da89d818
Dean Chester <dean@maquib.com>
2014-02-05 20:22:54 +0000

    Changed the sbox component initialisation.

--- feistelFunction.vhd
+++ feistelFunction.vhd
@@ -19,56 +19,56 @@ architecture arch of feistelFunction is
 		) ;
 	end component;
 	
-	component s8_box is
+	component S8Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s7_box is
+	component S7Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s6_box is
+	component S6Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s5_box is
+	component S5Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s4_box is
+	component S4Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s3_box is
+	component S3Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 	
-	component s2_box is
+	component S2Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
 		) ;
 	end component;
 
-	component s1_box is
+	component S1Box is
 		port (
 			sIN: IN std_logic_VECTOR(5 downto 0);
 			sOUT: OUT std_logic_VECTOR(3 downto 0)
@@ -97,14 +97,14 @@ architecture arch of feistelFunction is
 		eFunc : eFunction port map (fIn, e);
 		xorBIT : XORBIT48 port map (e, K, xorOUT); 
 	
-		s1 : s1_box port map (xorOUT(47 DOWNTO 42), sBoxesOUT(31 DOWNTO 28));
-		s2 : s2_box port map (xorOUT(41 DOWNTO 36), sBoxesOUT(27 DOWNTO 24));
-		s3 : s3_box port map (xorOUT(35 DOWNTO 30), sBoxesOUT(23 DOWNTO 20));
-		s4 : s4_box port map (xorOUT(29 DOWNTO 24), sBoxesOUT(19 DOWNTO 16));
-		s5 : s5_box port map (xorOUT(23 DOWNTO 18), sBoxesOUT(15 DOWNTO 12));
-		s6 : s6_box port map (xorOUT(17 DOWNTO 12), sBoxesOUT(11 DOWNTO 8));
-		s7 : s7_box port map (xorOUT(11 DOWNTO 6), sBoxesOUT(7 DOWNTO 4));
-		s8 : s8_box port map (xorOUT(5 DOWNTO 0), sBoxesOUT(3 DOWNTO 0));
+		s1 : S1Box port map (xorOUT(47 DOWNTO 42), sBoxesOUT(31 DOWNTO 28));
+		s2 : S2Box port map (xorOUT(41 DOWNTO 36), sBoxesOUT(27 DOWNTO 24));
+		s3 : S3Box port map (xorOUT(35 DOWNTO 30), sBoxesOUT(23 DOWNTO 20));
+		s4 : S4Box port map (xorOUT(29 DOWNTO 24), sBoxesOUT(19 DOWNTO 16));
+		s5 : S5Box port map (xorOUT(23 DOWNTO 18), sBoxesOUT(15 DOWNTO 12));
+		s6 : S6Box port map (xorOUT(17 DOWNTO 12), sBoxesOUT(11 DOWNTO 8));
+		s7 : S7Box port map (xorOUT(11 DOWNTO 6), sBoxesOUT(7 DOWNTO 4));
+		s8 : S8Box port map (xorOUT(5 DOWNTO 0), sBoxesOUT(3 DOWNTO 0));
 		
 		pFunc : permutationFunction PORT MAP (sBoxesOUT, fOut);
 end architecture ; -- arch
