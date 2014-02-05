a2d610de98126f5d5c3acfc81b86cacbcc42b5f8
Dean Chester <dean@maquib.com>
2014-02-05 16:28:35 +0000

    Fixed syntax error when -> case.

--- s2_box.vhd
+++ s2_box.vhd
@@ -12,7 +12,7 @@ end entity ; -- S2Box
 architecture arch of S2Box is
 
 begin
-	when sIN is:
+	case sIN is:
 		when to_unsigned(0,6) => sOUT <= to_unsigned(15, 4);
 		when to_unsigned(1,6) => sOUT <= to_unsigned(3, 4);
 		when to_unsigned(2,6) => sOUT <= to_unsigned(1, 4);
