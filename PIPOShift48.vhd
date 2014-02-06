ada9fdb8215aa3bd53b203fde6dbe05e0cee64ab
Dean Chester <dean@maquib.com>
2014-02-06 14:41:06 +0000

    Wrote a Shift Register 48-bit PIPO

---
+++ PIPOShift48.vhd
@@ -0,0 +1,22 @@
+library ieee ;
+	use ieee.std_logic_1164.all ;
+	use ieee.numeric_std.all ;
+
+entity PIPOShift32 is
+  port (
+	clock : IN STD_LOGIC;
+	dataIN : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	dataOUT : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+  ) ;
+end entity ; -- PIPOShift32
+
+architecture arch of PIPOShift32 is
+
+begin
+	process(clock)
+	begin 
+		if (clock'EVENT AND clock='1') then
+			dataOUT <= dataIN;
+		end if;
+	end process;
+end architecture ; -- arch
