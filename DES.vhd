d01f0d6664b9c77acaf41978ebac2fe5553c4a70
Dean Chester <dean@maquib.com>
2014-02-11 11:56:22 +0000

    Refactored module to allow the Wrapper to handle breaking of bits.

--- DES.vhd
+++ DES.vhd
@@ -4,44 +4,27 @@ library ieee ;
 
 entity DES is
   port (
-	clock : in STD_LOGIC;
-	plainTextB1 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB2 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB3 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB4 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB5 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB6 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB7 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
-	plainTextB8 : in STD_LOGIC_VECTOR(7 DOWNTO 0);
+		plainText : in STD_LOGIC_VECTOR(63 DOWNTO 0);
 	
-  	key1Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key2Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key3Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key4Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key5Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key6Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key7Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key8Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key9Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key10Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key11out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key12Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key13Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key14Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key15Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-  	key16Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
-	
-	cipherTextB1 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB2 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB3 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB4 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB5 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB6 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB7 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	cipherTextB8 : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-	
-	done : out STD_LOGIC;
-  ) ;
+		key1Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key2Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key3Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key4Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key5Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key6Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key7Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key8Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key9Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key10Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key11out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key12Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key13Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key14Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key15Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		key16Out : OUT STD_LOGIC_VECTOR(47 DOWNTO 0);
+		
+		cipherText : out STD_LOGIC_VECTOR(63 DOWNTO 0);
+	) ;
 end entity ; -- DES
 
 architecture arch of DES is
@@ -117,18 +100,9 @@ architecture arch of DES is
 		feistelResult9, feistelResult10, feistelResult11, feistelResult12, 
 		feistelResult13, feistelResult14, feistelResult15, feistelResult16,
 		feistelResult17 : STD_LOGIC_VECTOR(31 DOWNTO 0);
-	signal combined, permOut, inverseIn, inverseOut : STD_LOGIC_VECTOR(63 DOWNTO 0);
+	signal permOut, inverseIn, inverseOut : STD_LOGIC_VECTOR(63 DOWNTO 0);
 begin
-	combined(7 DOWNTO 0) <= plainTextB1;
-	combined(15 DOWNTO 8) <= plainTextB2;
-	combined(23 DOWNTO 16) <= plainTextB3;
-	combined(31 DOWNTO 24) <= plainTextB4;
-	combined(39 DOWNTO 32) <= plainTextB5;
-	combined(47 DOWNTO 40) <= plainTextB6;
-	combined(55 DOWNTO 48) <= plainTextB7;
-	combined(63 DOWNTO 56) <= plainTextB8;
-	
-	perm : initialPermutation PORT MAP(combined, permOut);
+	perm : initialPermutation PORT MAP(plainText, permOut);
 	
 	left1 <= permOut(31 DOWNTO 0);
 	right1 <= permOut(63 DOWNTO 32);
@@ -232,4 +206,8 @@ begin
 	
 	regFinalLeft : PIPOShift32 PORT MAP(leftResult16, inverseIn(31 DOWNTO 0));
 	regFinalRight : PIPOShift32 PORT MAP(rightResult16, inverseIn(63 DOWNTO 31));
+	
+	invPerm : inversePermutation PORT MAP(inverseIn, inverseOut);
+	
+	cipherText <= inverseOut;
 end architecture ; -- arch
