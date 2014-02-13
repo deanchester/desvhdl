b328b312cefb2d73f9b26730f5b821ea30bc16b8
Dean Chester <dean@maquib.com>
2014-02-13 09:55:41 +0000

    Changed variable names for keys in.

--- KeyContainer.vhd
+++ KeyContainer.vhd
@@ -5,22 +5,22 @@ library ieee ;
 entity KeyContainer is
   port (
 	clock, load : IN STD_LOGIC;
-	key1Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key2Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key3Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key4Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key5Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key6Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key7Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key8Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key9Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key10Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key11out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key12Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key13Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key14Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key15Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
-	key16Out : IN STD_LOGIC_VECTOR(47 DOWNTO 0)
+	key1In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key2In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key3In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key4In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key5In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key6In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key7In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key8In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key9In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key10In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key11In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key12In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key13In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key14In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key15In : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
+	key16In : IN STD_LOGIC_VECTOR(47 DOWNTO 0)
 	
 	keyOut : OUT STD_LOGIC_VECTOR(47 DOWNTO 0)
   ) ;
@@ -35,22 +35,22 @@ begin
 	process(clock)
 	begin 
 		if (clock'EVENT AND clock='1' AND load='1') then
-			sr1Out <= key1Out;
-			sr2Out <= key2Out;
-			sr3Out <= key3Out;
-			sr4Out <= key4Out;
-			sr5Out <= key5Out;
-			sr6Out <= key6Out;
-			sr7Out <= key7Out;
-			sr8Out <= key8Out;
-			sr9Out <= key9Out;
-			sr10Out <= key10Out;
-			sr11Out <= key11Out;
-			sr12Out <= key12Out;
-			sr13Out <= key13Out;
-			sr14Out <= key14Out;
-			sr15Out <= key15Out;
-			sr16Out <= key16Out;
+			sr1Out <= key1In;
+			sr2Out <= key2In;
+			sr3Out <= key3In;
+			sr4Out <= key4In;
+			sr5Out <= key5In;
+			sr6Out <= key6In;
+			sr7Out <= key7In;
+			sr8Out <= key8In;
+			sr9Out <= key9In;
+			sr10Out <= key10In;
+			sr11Out <= key11In;
+			sr12Out <= key12In;
+			sr13Out <= key13In;
+			sr14Out <= key14In;
+			sr15Out <= key15In;
+			sr16Out <= key16In;
 			keyOut <= sr1Out;
 		elsif (clock'EVENT AND clock='1' AND load='0') then
 			sr1Out <= sr2Out;
