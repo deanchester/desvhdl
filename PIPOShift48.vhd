0ef7b982378f30565170bbcf7b10781d979f0729
Dean Chester <dean@maquib.com>
2014-02-17 15:57:20 +0000

    Renamed entity and architecture.

--- PIPOShift48.vhd
+++ PIPOShift48.vhd
@@ -2,7 +2,7 @@ library ieee ;
 	use ieee.std_logic_1164.all ;
 	use ieee.numeric_std.all ;
 
-entity PIPOShift32 is
+entity PIPOShift48 is
   port (
 	clock : IN STD_LOGIC;
 	dataIN : IN STD_LOGIC_VECTOR(47 DOWNTO 0);
@@ -10,7 +10,7 @@ entity PIPOShift32 is
   ) ;
 end entity ; -- PIPOShift32
 
-architecture arch of PIPOShift32 is
+architecture arch of PIPOShift48 is
 
 begin
 	process(clock)
