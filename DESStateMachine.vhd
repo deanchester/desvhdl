d6cc77aedaa8d3b218129d518484c49f63ce3412
Dean Chester <dean@maquib.com>
2014-02-17 10:13:12 +0000

    Made changes to signals and I/O ports.

--- DESStateMachine.vhd
+++ DESStateMachine.vhd
@@ -6,13 +6,17 @@ entity DESStateMachine is
   port (
 	clock, reset : IN STD_LOGIC;
 	select1, select2, load : OUT STD_LOGIC;
-	key_select : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+	complete : OUT STD_LOGIC
   ) ;
 end entity ; -- DESStateMachine
 
 architecture arch of DESStateMachine is
-	type STATETYPE is (A,B,C,D,E,F,G,H,I,J,K,L);
+	type STATETYPE is (SCHEDULE_KEYS, LOAD_KEYS, R1Load, R1Save, R2Load, R2Save,
+		R3Load, R3Save, R4Load, R4Save, R5Load, R5Save, R6Load, R6Save, R7Load,
+		R7Save, R8Load, R8Save, R9Load, R9Save, R10Load, R10Save, R11Load,
+		R11Save, R12Load, R12Save, R13Load, R13Save, R14Load, R14Save, R15Load,
+		R15Save, R16Load, R16Save, Result);
 	signal State : STATETYPE;
 begin
-	select1 <= '1' when (State = C, D, E, G, H, I)
+	
 end architecture ; -- arch
