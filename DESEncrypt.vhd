f00083c9b44ce7597ea5cfc8d07c023e2b2104d2
Dean Chester <dean@maquib.com>
2014-02-17 16:11:57 +0000

    Feistel FuncOut is now the output of the feistel function.

--- DESEncrypt.vhd
+++ DESEncrypt.vhd
@@ -133,7 +133,7 @@ architecture arch of DESEncrypt is
 		sr1Left : PIPOShift32 PORT MAP(clock, muxLeftOut, srLeftOut);
 		sr1Right : PIPOShift32 PORT MAP(clock, muxRightOut, srRightOut);
 		
-		feistelFunc : feistelFunction PORT MAP(srRightOut, keyOut, newLeft);
+		feistelFunc : feistelFunction PORT MAP(srRightOut, keyOut, feistelFuncOut);
 		xorMod : XORBit32 PORT MAP(srLeftOut, feistelFuncOut, newRight);
 		
 		sr2Left : PIPOShift32 PORT MAP(clock, newLeft, newLeftSROut);
