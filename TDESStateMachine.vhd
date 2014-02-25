ebe1a2b6be264fcd1c3c97c411c8baf43eacf78f
Dean Chester <dean@maquib.com>
2014-02-25 11:54:03 +0000

    Inital commit of the statemachine for the TDES algorithm.

---
+++ TDESStateMachine.vhd
@@ -0,0 +1,16 @@
+library ieee ;
+	use ieee.std_logic_1164.all ;
+	use ieee.numeric_std.all ;
+
+entity TDESStateMachine is
+  port (
+	clock : IN STD_LOGIC;
+	done : OUT STD_LOGIC;
+  ) ;
+end entity ; -- TDESStateMachine
+
+architecture arch of TDESStateMachine is
+
+begin
+
+end architecture ; -- arch
