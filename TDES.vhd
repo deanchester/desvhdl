6a87aaa400cf7109cf2d77acd47d9396d3dd5a00
Dean Chester <dean@maquib.com>
2014-02-25 11:48:54 +0000

    Added components required for TDES.

--- TDES.vhd
+++ TDES.vhd
@@ -12,7 +12,22 @@ entity TDES is
 end entity ; -- TDES
 
 architecture arch of TDES is
-
+	COMPONENT DES is
+		port (
+			clock, start, reset, decryption : IN STD_LOGIC; 
+			plainText, key : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+			done : OUT STD_LOGIC;
+			ciperText : OUT STD_LOGIC_VECTOR(63 DOWNTO 0)
+		) ;
+	end COMPONENT;
+	
+	COMPONENT KeyStore is
+	  port (
+		clock : IN STD_LOGIC;
+		keyIn : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
+		keyOut : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);
+	  ) ;
+	end COMPONENT;
 begin
 
 end architecture ; -- arch
