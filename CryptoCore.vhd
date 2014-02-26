fecb3532b4bcdf3711946fc7c5f0b8f861c07dec
Dean Chester <dean@maquib.com>
2014-02-26 13:54:47 +0000

    Initial commit for the cryptocore module. With template.

---
+++ CryptoCore.vhd
@@ -0,0 +1,15 @@
+library ieee ;
+	use ieee.std_logic_1164.all ;
+	use ieee.numeric_std.all ;
+
+entity CryptoCore is
+  port (
+	clock
+  ) ;
+end entity ; -- CryptoCore
+
+architecture arch of CryptoCore is
+
+begin
+
+end architecture ; -- arch
