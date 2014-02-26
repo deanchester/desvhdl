8de3971042f25c50d195021a10c742d43f5ee375
Dean Chester <dean@maquib.com>
2014-02-26 11:42:23 +0000

    Wrote the algorithm decode module.

--- AlgorithmDecode.vhd
+++ AlgorithmDecode.vhd
@@ -6,12 +6,33 @@ entity AlgorithmDecode is
   port (
 	clock : IN STD_LOGIC;
 	algorithmSelect : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-	mode1, mode2, mode3 : OUT STD_LOGIC
+	mode, mode1, mode2, mode3: OUT STD_LOGIC
   ) ;
 end entity ; -- AlgorithmDecode
 
 architecture arch of AlgorithmDecode is
-
 begin
-
+	process(algorithmSelect)
+	begin
+		case algorithmSelect is 
+			when "00" => 
+				mode := 1;
+				mode1 := 0;
+			when "01" => 
+				mode := 1;
+				mode1 := 1;
+			when "10" => 
+				mode := 0;
+				mode1 := 0;
+				mode2 := 1;
+				mode3 := 0;
+			when "11" => 
+				mode := 0;
+				mode1 := 1;
+				mode2 := 0;
+				mode3 := 1;
+		end case;
+	end process;
+	
+	
 end architecture ; -- arch
