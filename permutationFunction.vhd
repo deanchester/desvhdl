504d0f633b53bfab496874a4271fdc6d2c9754c0
Dean Chester <dean@maquib.com>
2014-03-12 21:00:18 +0000

    Switched endianess to BIG ENDIANESS hopefully will fix it.

--- permutationFunction.vhd
+++ permutationFunction.vhd
@@ -12,43 +12,36 @@ end entity ; -- permutationFunction
 architecture arch of permutationFunction is
 
 begin
-	permOUT(0)  <= permIN(15);
-	permOUT(1)  <= permIN(6);
-	permOUT(2)  <= permIN(19);	
-	permOUT(3)  <= permIN(20);
-	
-	permOUT(4)  <= permIN(28);
-	permOUT(5)  <= permIN(11);
-	permOUT(6)  <= permIN(27);	
-	permOUT(7)  <= permIN(16);
-	
-	permOUT(8)  <= permIN(0);
-	permOUT(9)  <= permIN(14);
-	permOUT(10)  <= permIN(22);	
-	permOUT(11)  <= permIN(25);
-	
-	permOUT(12)  <= permIN(4);
-	permOUT(13)  <= permIN(17);
-	permOUT(14)  <= permIN(30);	
-	permOUT(15)  <= permIN(9);
-	
-	permOUT(16)  <= permIN(1);
-	permOUT(17)  <= permIN(7);
-	permOUT(18)  <= permIN(23);	
-	permOUT(19)  <= permIN(13);
-	
-	permOUT(20)  <= permIN(31);
-	permOUT(21)  <= permIN(26);
-	permOUT(22)  <= permIN(2);	
-	permOUT(23)  <= permIN(8);
-	
-	permOUT(24)  <= permIN(18);
-	permOUT(25)  <= permIN(12);
-	permOUT(26)  <= permIN(29);	
-	permOUT(27)  <= permIN(5);
-	
-	permOUT(28)  <= permIN(21);
-	permOUT(29)  <= permIN(10);
-	permOUT(30)  <= permIN(3);	
-	permOUT(31)  <= permIN(24);
+	permOUT(31) <= permIN(15);
+	permOUT(30) <= permIN(6);
+	permOUT(29) <= permIN(19);	
+	permOUT(28) <= permIN(20);
+	permOUT(27) <= permIN(28);
+	permOUT(26) <= permIN(11);
+	permOUT(25) <= permIN(27);	
+	permOUT(24) <= permIN(16);	
+	permOUT(23) <= permIN(0);
+	permOUT(22) <= permIN(14);
+	permOUT(21)  <= permIN(22);	
+	permOUT(20)  <= permIN(25);
+	permOUT(19)  <= permIN(4);
+	permOUT(18)  <= permIN(17);
+	permOUT(17)  <= permIN(30);	
+	permOUT(16)  <= permIN(9);	
+	permOUT(15)  <= permIN(1);
+	permOUT(14)  <= permIN(7);
+	permOUT(13)  <= permIN(23);	
+	permOUT(12)  <= permIN(13);
+	permOUT(11)  <= permIN(31);
+	permOUT(10)  <= permIN(26);
+	permOUT(9)  <= permIN(2);	
+	permOUT(8)  <= permIN(8);
+	permOUT(7)  <= permIN(18);
+	permOUT(6)  <= permIN(12);
+	permOUT(5)  <= permIN(29);	
+	permOUT(4)  <= permIN(5);
+	permOUT(3)  <= permIN(21);
+	permOUT(2)  <= permIN(10);
+	permOUT(1)  <= permIN(3);	
+	permOUT(0)  <= permIN(24);
 end architecture ; -- arch
